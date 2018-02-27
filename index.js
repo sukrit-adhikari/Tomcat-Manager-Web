949e4fd1c0aea36233a9957076dc59c7ba4329a8
sukriticc <sadhikari@icct.com>
2018-02-27 14:14:09 -0500

    catalina run in spawn and mvn build and watch folder

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 // import {AssetCopy} from "./modules/AssetCopy.js";
 
 const express = require('express');
+var compression = require('compression');
 const moment = require("moment");
 const path = require("path");
+const fs = require("fs");
+const getFolderSize = require('get-folder-size');
+const md5 = require('md5');
+const ncp = require('ncp').ncp;
+ncp.limit = 16;
 
 // const assetCopy = AssetCopy();
 const app = express();
 const router = express.Router();
 const port = 9090;
+const logFile = './manager.log';
+
+const folderSize = {};
+const assetCopyTimer = {};
 
 app.use(express.static('public'));
+app.use(compression());
+
+var logStream = fs.createWriteStream(logFile, {flags: 'w'});
+
+// const WebSocket = require('ws');
+// const wss = new WebSocket.Server({
+//   port: 9091,
+// });
 
 app.get('/',function(req,res){
     res.sendFile(path.resolve('./public/view/index.html'));
@@ -20,64 +38,137 @@ app.get('/api/project',function(req,res){
 });
 
 app.get('/api/tomcat/start',function(req,res){
-    var exec = require('child_process').exec;
-    exec('startup', {
-    cwd: 'C:\\development\\tomcat-1\\bin'
-    }, function(error, stdout, stderr) {
-        
+    var spawn = require('child_process').spawn;
+
+    var execution = spawn('catalina run', {
+    cwd: req.query.tomcatBin,
+    silent:true,
+    maxBuffer: 1024 * 1000,
+    shell:true,
+    });
+
+    execution.stdout.resume();
+    execution.stderr.resume();
+
+    execution.stdout.on('data', function(data) {
+        logStream.write(data);
+        console.log(data.length+" bytes output from tomcat.");
+    });
+    execution.stderr.on('data', (data) => {
+        logStream.write(data);
+        console.log(data);
+    });
+    execution.on('close', (code) => {
+        logStream.write(code);
+        console.log(code);
     });
     res.send({startup:new Date()});
 });
 
-app.get('/api/tomcat/stop',function(req,res){
-    var exec = require('child_process').exec;
-    exec('catalina stop -force', {
-    cwd: 'C:\\development\\tomcat-1\\bin'
-    }, function(error, stdout, stderr) {
-        console.log(error,stdout,stderr);
-        if(error || stderr){
-            if(error){
-                res.send({failed:true, error:error});
-            }else{
-                res.send({failed:true, stderr:stderr});
-            }
+
+app.get('/api/build',function(req,res){
+    var location = req.query.location;
+    var spawn = require('child_process').spawn;
+    var execution = spawn('mvn package', {
+    cwd: location,
+    silent:true,
+    maxBuffer: 1024 * 300,
+    shell:true,
+    });
+    // execution.stdout.pipe(logStream);
+    execution.stdout.on('data', function(data) {
+        logStream.write(data);
+        console.log(data);
+    });
+});
+
+app.get('/api/copy/file',function(req,res){
+    var from = req.query.from.replace(/\\\\/g, '\\');
+    var to = req.query.to.replace(/\\\\/g, '\\');
+    try{
+        var fromParts = from.split("\\");
+        var fileName = fromParts[fromParts.length -1];
+        fs.copyFileSync(from, path.join(to,fileName));
+        var message = {action:"copyFile",success:true,from:from,to:to};
+        logStream.write(JSON.stringify(message)+'\n');
+        res.send(message);
+    }catch(error){
+        logStream.write(JSON.stringify(error)+'\n');
+        res.send(error);
+    }
+});
+
+app.get('/api/log',function(req,res){
+    const stats = fs.statSync(logFile);
+    const fileSizeInBytes = stats.size;
+    res.setHeader('X-LOG-FILE-SIZE',fileSizeInBytes);
+    if(req.query.previousSize !== null){
+        if(parseInt(req.query.previousSize,10) === parseInt(fileSizeInBytes,10)){
+            res.send({noChange:true});
             return;
         }
-        res.send({shutdown:new Date(),output:stdout});
-    });
+    }
+    res.sendFile(path.resolve(logFile));
 });
 
-app.get('/copy',function(req,res){
-    // const ncp = require('ncp').ncp;
-    // ncp.limit = 16;
-
-    // ncp("C:\\development\\repos\\bids-tool\\SarnovaBid\\SarnovaBid-Web\\target\\SarnovaBid-Web\\resources", "C:\\development\\tomcat-1\\webapps\\SarnovaBid-Web\\resources", function (err) {
-    //     if (err) {
-    //       return console.error(err);
-    //     }
-    //     console.log('done!');
-    //     res.send("DONE!!!")
-    // });
-
-    // var exec = require('child_process').exec;
-    // exec('mvn package', {
-    // cwd: 'C:\\development\\repos\\bids-tool\\SarnovaBid\\SarnovaBid-Web'
-    // }, function(error, stdout, stderr) {
-    //     res.send(stdout);
-    // });
-
-
-
+app.get('/api/tomcat/stop',function(req,res){
     var exec = require('child_process').exec;
-    exec('shutdown', {
-    cwd: 'C:\\development\\tomcat-1\\bin'
-    }, function(error, stdout, stderr) {
-        // console.log(error,stdout,stderr);
-        res.send(stdout);
-        // res.send(stderr);
-    });
+    var execution = exec('catalina stop -force', {
+        cwd: req.query.tomcatBin,
+        }, function(error, stdout, stderr) {
+            console.log(error,stdout,stderr);
+            // stdout = stdout.replace(new RegExp('\r?\n','g'), '<br />');
+            // stderr = stderr.replace(new RegExp('\r?\n','g'), '<br />');
+            if(error || stderr){
+                if(error){
+                    logStream.write(JSON.stringify(error));
+                    res.send({failed:true, error:error});
+                }else{
+                    logStream.write(stderr);
+                    res.send({failed:true, stderr:stderr});
+                }
+                return;
+            }
+            res.send({shutdown:new Date(),output:stdout});
+        });
 });
 
+app.get('/api/copy/folder',function(req,res){
+    var from =  req.query.from.replace(/\\\\/g, '\\');
+    var to = req.query.to.replace(/\\\\/g, '\\');
+
+    var timerHash = md5(from+to);
+    clearInterval(assetCopyTimer[timerHash]);
+
+    assetCopyTimer[timerHash] = setInterval(
+            function(){
+                getFolderSize(from, function(err,size){
+                    if(err){
+                        logStream.write(err);
+                        return;
+                    }
+                    console.log("Size "+size+" Folder "+from);
+                    if(folderSize[md5(from)] === size){ // no change no copy
+                        return;
+                    }
+                    folderSize[md5(from)] = size;
+                    ncp(from, to , function (err) {
+                        if (err) {
+                        var response = {success:false,error:err,from:from,to:to};
+                        logStream.write(JSON.stringify(response)+'\n');
+                        console.log(response);
+                        }else{
+                            var response = {success:true,from:from,to:to};
+                            logStream.write(JSON.stringify(response)+'\n');
+                            console.log(response);
+                        }
+                    });
+                })
+            },
+            2000);
+
+    res.send({message:"Copy request received."});
+});
 
 app.listen(port);
 console.log('Tomcat Manager Started at PORT:',port);
--- public/js/project.js
+++ public/js/project.js
@@ -6,9 +6,29 @@ new Vue({
                     bin:null
                 }
             },
+      state:{
+          building:false
+      },
+      timer: {
+        loadLog: null,
+        watchFolder: {},
+      },
+      watching:{},
+      log: "",
+    },
+    watch: {
+        log: function (val) {
+            // setTimeout(
+            //     function(){
+                   
+            //     }
+            //     ,500);
+        }
     },
     mounted: function () {
         var self = this;
+        self.loadLog();
+        
         $.ajax({
             url: '/api/project',
             method: 'GET',
@@ -21,49 +41,138 @@ new Vue({
         });
     },
     methods:{
+        isBuilding: function(){
+            return this.state.building;
+        },
         populateModal : function(text){
             $('#responseReceivedModal .modal-body').html(text);
         },
         showModal: function(){
+            this.populateModal("Loading...");
             $('#responseReceivedModal').modal('show');
         },
-        sendTomcatStopRequest: function(){
+        sendTomcatStopRequest: function(tomcatBin){
             var self = this;
+            // self.showModal();
             $.ajax({
                 url: '/api/tomcat/stop',
                 method: 'GET',
+                data:{tomcatBin:tomcatBin},
                 success: function (response) {
-                    if(response.failed && response.stderr){
-                        self.populateModal(response.stderr);
+                    // if(response.failed && response.stderr){
+                    //     self.log+=response.stderr;
+                    // }
+                    // if(response.failed && response.error){
+                    //     self.log+=response.error;
+                    // }
+                    // if(response.shutdown){
+                    //     self.log+=response.shutdown;
+                    // }
+                },
+                error: function (error) {
+                    self.log+="<CONSOLE> Stopping Tomcat Failed...\n";
+                    console.log(JSON.stringify(error));
+                }
+            });
+        },
+        sendTomcatStartRequest: function(tomcatBin){
+            var self = this;
+            // self.showModal();
+            $.ajax({
+                url: '/api/tomcat/start',
+                method: 'GET',
+                data:{tomcatBin:tomcatBin},
+                success: function (response) {
+                    // if(response.startup){
+                    //     self.populateModal(response.startup);
+                    // }
+                },
+                error: function (error) {
+                    self.log+="<CONSOLE> Starting Tomcat Failed...\n";
+                    console.log(JSON.stringify(error));
+                }
+            });
+        },
+        loadLog: function(){
+            var self = this;
+            clearInterval(this.timer.loadLog);
+            this.timer.loadLog = setInterval(function(){
+                $.ajax({
+                    method: 'GET',
+                    url: '/api/log',
+                    data: {previousSize:self.log.length === 0 ? -1 : self.log.length },
+                    success: function (response, textStatus, jqXHR) {
+                        if(!response.noChange && self.log.length !== jqXHR.getResponseHeader("X-LOG-FILE-SIZE")){
+                            self.log = response;
+                        }
+                        if(!$("#log").is(":focus")){
+                            $("#log").animate({
+                                scrollTop:$("#log")[0].scrollHeight - $("#log").height()
+                            },200,function(){
+                                // success clbck           
+                            })
+                        }
+                    },
+                    error: function (error) {
+                        self.log+="<CONSOLE> Loading Log Failed...\n";
+                        console.log(JSON.stringify(error));
                     }
-                    if(response.failed && response.error){
-                        self.populateModal(response.error);
-                    }
-                    if(response.shutdown){
-                        self.populateModal(response.shutdown);
-                    }
-                    self.showModal();
+                });
+            },2000);
+            
+        },
+        sendBuildRequest: function(location){
+            var self = this;
+            this.state.building = true;
+            $.ajax({
+                url: '/api/build',
+                method: 'GET',
+                data:{location:location},
+                success: function (response) {
+                    self.state.building = false;
+                },
+                error: function (error) {
+                    self.state.building = false;
+                    alert(JSON.stringify(error));
+                }
+            });
+        },
+        watchFolder: function(from,to){
+            var self = this;
+            var timerHash = md5(from+to);
+            this.watching[timerHash] = true;
+            $.ajax({
+                url: '/api/copy/folder',
+                method: 'GET',
+                data:{from:from,to:to},
+                success: function (response) {
+                    self.populateModal(response.message);
                 },
                 error: function (error) {
                     alert(JSON.stringify(error));
                 }
             });
         },
-        sendTomcatStartRequest: function(){
+        copyFile: function(from,to){
             var self = this;
             $.ajax({
-                url: '/api/tomcat/start',
+                url: '/api/copy/file',
                 method: 'GET',
+                data:{from:from,to:to},
                 success: function (response) {
-                    if(response.startup){
-                        self.populateModal(response.startup);
-                    }
-                    self.showModal();
+                    
                 },
                 error: function (error) {
                     alert(JSON.stringify(error));
                 }
             });
+        },
+        isWatching: function(from,to){
+            var hash = md5(from+to);
+            if(this.watching[hash]){
+                return true;
+            }
+            return false;
         }
     }
 });
